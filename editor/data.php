6d7fe77f43f6a721a3963612edc5979f19240184
Aman <aman.shygaibaev@gmail.com>
2020-06-04 23:32:18 +0300

    В этом коммите, добавление id студентов работает не корркектно

--- editor/data.php
+++ editor/data.php
@@ -6,7 +6,7 @@
 
 $data=json_decode(file_get_contents('php://input'), true);
 
-if(isset($data)){
+
 if(isset($data)){
   echo ('filled data');
 }else{
@@ -17,7 +17,7 @@ print_r($data);
 $nameOfCourse=$data['quadro']['title']; //Списки подгрупп такого та курса;
 $yearOfCourse=$data['quadroinfo'][0]['year']; //Год курса;
 $idCourse=$data['quadroinfo'][0]['id']; //индекс курса 
-
+$checkyearOfCourse=$yearOfCourse;
 
 $hello=count($data['cards']);
 $hello1=count($data['cards'][0]['tarefas']);
@@ -34,25 +34,37 @@ for($index=0;$index<count($data['cards']);$index++){
   $groupname=$data['cards'][$index]['name']; //название группы;
   $sendtogroup="INSERT INTO `groups`(`name`,`year`)VALUES('$groupname','$yearOfCourse')";
   mysqli_query($connection,$sendtogroup);
-  if($yearOfCourse===2019){
-    $getgroupid2019="SELECT `id` FROM `groups` WHERE `year`=2019 ";
+}
+  if($yearOfCourse===$checkyearOfCourse){
+    $getgroupid2019="SELECT `id` FROM `groups` WHERE `year`=$checkyearOfCourse ";
     $storage2019=mysqli_query($connection,$getgroupid2019);
-  while($assoccStorage2019=mysqli_fetch_assoc($storage2019)){
+    while($assoccStorage2019=mysqli_fetch_assoc($storage2019)){
     $groupID2019=$assoccStorage2019['id'];
+    $check=true;
+    $index++;
+  for($index=0;$index<count($data['cards']);$index++){
+    if($check){
   for($index1=0;$index1<count($data['cards'][$index]['tarefas']);$index1++){
     $studentnames=$data['cards'][$index]['tarefas'][$index1]['name'];
     $sendstudents="INSERT INTO `students`(`first_name`)VALUES('$studentnames')";
     mysqli_query($connection,$sendstudents);
-    $sendIDgroup="INSERT INTO `students_to_group`(`group_id`)VALUES('$groupID2019')";
-    mysqli_query($connection,$sendIDgroup);
+    $getStudentID="SELECT `id` FROM `students`";
+    $studentrequest=mysqli_query($connection,$getStudentID);
+    $studentstorage=mysqli_fetch_assoc($studentrequest);
+    $sendIDstudent=$studentstorage['id'];
+    $sendAllID="INSERT INTO `students_to_group`(`group_id`,`student_id`)VALUES('$groupID2019','$sendIDstudent')";
+    mysqli_query($connection,$sendAllID);
   }
-  }
-
-  }
+  $check=false;
 }
 
-
 }
+    }
+  }
+
+
+
+
 
 // расписать if для getgroupid, создать переменную(есть), которая будет хранить в себе год.
 // $studentId=$data['cards'][$index]['tarefas'][$index1]['id'];
